d39fa911646f66da6426fead41ee23265e65ddcd
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-04-05 01:58:09 +0300

    feat: add react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css'
+import { BrowserRouter } from "react-router-dom";
+import 'normalize.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
